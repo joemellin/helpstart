346e6abb62a8c51a1f8a2128f4439220bd5b8f8f
Joe Mellin <mellin.joe@gmail.com>
2013-04-25 18:48:31 +0800

    comment out mail

--- config/initializers/mail_setup.rb
+++ config/initializers/mail_setup.rb
@@ -1,9 +1,9 @@
-ActionMailer::Base.smtp_settings = {
-:address => ‘smtp.sendgrid.net’,
-:port => ’587′,
-:authentication => :plain,
-:user_name => ENV['app12784085@heroku.com'],
-:password => ENV['rx5j3oqo'],
-:domain => ‘heroku.com’
-}
-ActionMailer::Base.delivery_method = :smtp
+#ActionMailer::Base.smtp_settings = {
+#:address => ‘smtp.sendgrid.net’,
+#:port => ’587′,
+#:authentication => :plain,
+#:user_name => ENV['app12784085@heroku.com'],
+#:password => ENV['rx5j3oqo'],
+#:domain => ‘heroku.com’
+#}
+#ActionMailer::Base.delivery_method = :smtp
