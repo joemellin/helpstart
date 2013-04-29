039eab80eec573d8913fc3a534e183938cd656cb
Joe Mellin <mellin.joe@gmail.com>
2013-04-29 20:00:13 +0800

    change update to user profile

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,6 +4,9 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     user_path(current_user)
   end
+  def after_update_path_for(resource)
+    user_path(current_user)
+  end 
 
 
 
